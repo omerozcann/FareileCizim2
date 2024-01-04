1aa5138aa4127e7442a23c7033440e2dc34556cc
Öğr.Gör.Ömer Özcan <69793480+omerozcann@users.noreply.github.com>
2024-01-04 15:23:31 +0300

    Proje dosyası ekle.

---
+++ FareileCizim2/cizimFrm.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace FareileCizim2
+{
+    partial class cizimFrm
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.silgiBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // silgiBtn
+            // 
+            this.silgiBtn.Location = new System.Drawing.Point(49, 509);
+            this.silgiBtn.Name = "silgiBtn";
+            this.silgiBtn.Size = new System.Drawing.Size(75, 65);
+            this.silgiBtn.TabIndex = 0;
+            this.silgiBtn.Text = "Silgi";
+            this.silgiBtn.UseVisualStyleBackColor = true;
+            this.silgiBtn.Click += new System.EventHandler(this.silgiBtn_Click);
+            // 
+            // cizimFrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1151, 622);
+            this.Controls.Add(this.silgiBtn);
+            this.Name = "cizimFrm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Çizim";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.cizimFrm_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.cizimFrm_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.cizimFrm_MouseUp);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button silgiBtn;
+    }
+}
+
---
+++ FareileCizim2/cizimFrm.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FareileCizim2
+{
+    public partial class cizimFrm : Form
+    {
+        int x1,y1,x2,y2,t,s=0;
+        Color fc;
+        Pen p = new Pen(Color.Red, 3);
+        Pen pForm;
+        SolidBrush sb;
+        private void cizimFrm_MouseDown(object sender, MouseEventArgs e)
+        {
+            x1 = e.X;
+            y1 = e.Y;
+            t = 1;
+        }
+
+        private void silgiBtn_Click(object sender, EventArgs e)
+        {
+            if(s== 0)
+            {
+                s = 1;
+                silgiBtn.BackColor = Color.Blue;
+            }
+            else
+            {
+                s = 0;
+                silgiBtn.BackColor = this.BackColor;
+            }
+        }
+
+        private void cizimFrm_MouseUp(object sender, MouseEventArgs e)
+        {
+            t = 0;
+            if (s == 0)
+            {
+                g.DrawLine(p, x1, y1, x2, y2);
+            }
+        }
+
+        Graphics g;
+        private void cizimFrm_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (s == 0)
+            {
+                if (t == 1)
+                {
+                    x2 = e.X;
+                    y2 = e.Y;
+                    g.DrawLine(p, x1, y1, x2, y2);
+                    g.DrawLine(pForm, x1, y1, x2, y2);
+                }
+            }
+            else
+            {
+                x2 = e.X;
+                y2 = e.Y;
+                g.FillEllipse(sb, x2-5, y2-5, 10, 10);                
+            }
+        }
+
+        public cizimFrm()
+        {
+            InitializeComponent();
+            g = this.CreateGraphics();
+            fc = this.BackColor;
+            pForm=new Pen(fc,3);
+            sb = new SolidBrush(fc);
+        }     
+    }
+}
